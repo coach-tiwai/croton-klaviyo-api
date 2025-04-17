b7b7e2ff20c55a2766eacb2fc696005b4f07262b
coach-tiwai <training@coachtiwai.com>
2025-04-18 07:27:46 +1200

    Update subscribeToKlaviyo.js

--- pages/api/subscribeToKlaviyo.js
+++ pages/api/subscribeToKlaviyo.js
@@ -27,7 +27,8 @@ export default async function handler(req, res) {
               {
                 email,
                 first_name,
-                last_name
+                last_name,
+                consent: ['EMAIL']
               }
             ]
           }
